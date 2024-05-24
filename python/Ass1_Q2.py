c7f74570ac53de9792aaa8b101cd5be39faf70f6
bankcheck <bankcheck@hotmail.com>
2024-05-25 04:32:23 +0800

    completed assignment

--- python/Ass1_Q2.py
+++ python/Ass1_Q2.py
@@ -1,3 +1,16 @@
-# user input i
- i = int("Input an integer: ")
- 
+# user input last
+last = int(input("Input an integer: "))
+
+# result
+result = 1
+
+#print the first part of the output
+print("Sum from 1 to", last, " = 1", end="")
+
+#calculate the result and print the middle part of the output
+for i in range(2, last + 1):
+    result += i
+    print(" +", i, end="")
+
+#print the last past of the output
+print(" =", result)
