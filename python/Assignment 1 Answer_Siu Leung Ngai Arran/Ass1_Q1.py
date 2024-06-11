37e87c1b61f08917f5e997a8b17932365efdc0b3
bankcheck <bankcheck@hotmail.com>
2024-06-11 11:07:28 +0800

    amend q3

--- python/Assignment 1 Answer_Siu Leung Ngai Arran/Ass1_Q1.py
+++
@@ -1,8 +0,0 @@
-drink = int(input("How many you want to buy? "))
-
-if drink <= 0:
-    print ("Invalid value")
-elif (drink > 0 and drink < 10):
-    print ("Delivery fee: $50")
-else:
-    print ("Free delivery")
