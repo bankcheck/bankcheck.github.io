fff8e86ea114fdb5e74c1bb471da96340f60f5f0
bankcheck <bankcheck@hotmail.com>
2024-10-01 02:19:59 +0800

    ok

--- python/m2/20240821/Lesson 5 - Code-20240821/L5-Ex7-selenium.py
+++ python/m2/20240821/Lesson 5 - Code-20240821/L5-Ex7-selenium.py
@@ -1,7 +1,7 @@
 import time
 from selenium import webdriver
 
-driver = webdriver.Chrome('C:\Drivers\chromedriver')
+driver = webdriver.Chrome(r'C:\Drivers\chromedriver.exe')
 driver.get("https://www.python.org")
 print(driver.title)
 print(driver.current_url)
