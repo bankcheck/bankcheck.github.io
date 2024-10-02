c30579111a73f677c96f5793f04e16716f72764b
bankcheck <bankcheck@hotmail.com>
2024-10-02 11:36:26 +0800

    commit

--- python/m2/assignment 2/login - Siu Leung Ngai Arran.py
+++
@@ -1,9 +0,0 @@
-import time
-from selenium import webdriver
-
-driver = webdriver.Chrome('C:\Drivers\chromedriver')
-driver.get("https://www.python.org")
-print(driver.title)
-print(driver.current_url)
-time.sleep(60)    #sleep for 60s
-driver.close()
